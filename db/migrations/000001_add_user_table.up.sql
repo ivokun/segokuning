5032759e5849b1fb10cccad08dfe88ee4f84bd43
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2024-03-19 16:28:46 +0700

    feat: add user table

---
+++ db/migrations/000001_add_user_table.up.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS users(
+  id TEXT PRIMARY KEY,
+  name VARCHAR (50) NOT NULL,
+  email VARCHAR (50) UNIQUE NOT NULL,
+  phone VARCHAR (50) UNIQUE NOT NULL,
+  password VARCHAR (255) NOT NULL,
+  created_at TIMESTAMP DEFAULT (TIMEZONE('UTC', NOW())),
+  modified_at TIMESTAMP DEFAULT (TIMEZONE('UTC', NOW())),
+);
+
+-- Trigger to update modified_at column
+CREATE OR REPLACE FUNCTION update_users_modified_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.modified_at = TIMEZONE('UTC', NOW());
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_users_modified_at_column_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_users_modified_at_column();
+
